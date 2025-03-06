9a5138034d421103d4309f5637f24f5f9a67897c
oussfer1 <sgtef.sarl@gmail.com>
2025-03-06 22:42:02 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, send_file
-from generate_doc import generate_word, generate_pdf
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,17 +6,5 @@ app = Flask(__name__)
 def formulaire():
     return render_template('formulaire.html')
 
-@app.route('/generate', methods=['POST'])
-def generate():
-    data = request.form.to_dict(flat=False)
-    file_format = request.form.get('format', 'word')
-
-    if file_format == 'pdf':
-        file_path = generate_pdf(data)
-    else:
-        file_path = generate_word(data)
-
-    return send_file(file_path, as_attachment=True)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
